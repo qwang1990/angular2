43d2d479ec68da0baf0331bdeb9dd61b4380b987
user <user@userdeMacBook-Pro.local>
2017-03-27 20:50:17 +0800

    form

--- angular-forms/src/app/hero-form.component.ts
+++ angular-forms/src/app/hero-form.component.ts
@@ -5,7 +5,7 @@ import { Hero }    from './hero';
 @Component({
   moduleId: module.id,
   selector: 'hero-form',
-  templateUrl: './hero-form.component.html'
+  templateUrl: 'hero-form.component.html'
 })
 export class HeroFormComponent {
 
@@ -20,4 +20,9 @@ export class HeroFormComponent {
 
   // TODO: Remove this when we're done
   get diagnostic() { return JSON.stringify(this.model); }
+
+  newHero() {
+    this.model = new Hero(42, '', '');
+  }
+
 }
